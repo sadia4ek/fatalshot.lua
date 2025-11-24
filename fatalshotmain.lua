078debec75f92aa86cd0765c3bbe3e183a19af57
sadia4ek <kapybara1122@gmail.com>
2025-11-24 20:31:55 +0200

    Create fatalshotmain.lua

---
+++ fatalshotmain.lua
@@ -0,0 +1,14 @@
+local Ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/Akiri%20Lib/Akiri%20Lib%20Source.lua"))()
+local Ui = Library
+
+Library.CreateLoader(
+    "loader", 
+    Vector2.new(300, 250)
+);
+local menuwindow = Library.Window(
+    "Window1", 
+    Vector2.new(524, 550)
+);
+
+local tab = Window1:Tab("1222121")
+local section = Tab1:Section("edwd", "Left")
