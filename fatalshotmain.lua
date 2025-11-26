f35ec2b541b97f8b75fe8aab9068eb043025a78b
sadia4ek <kapybara1122@gmail.com>
2025-11-26 15:05:47 +0200

    Update fatalshotmain.lua

--- fatalshotmain.lua
+++ fatalshotmain.lua
@@ -1,14 +1,69 @@
-local Ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/Akiri%20Lib/Akiri%20Lib%20Source.lua"))()
-local Ui = Library
+local Decimals = 4
+local Clock = os.clock()
+local ValueText = "11valueww"
+local WordList = {
+    "ez clap",
+    "no chance",
+    "outplayed",
+    "gg ez",
+    "too fast",
+    "you can't hit me",
+    "auto win",
+    "skill issue",
+    "next time maybe",
+    "try harder",
+    "no mercy",
+    "too strong",
+    "unstoppable",
+    "dominated",
+}
 
-Library.CreateLoader(
-    "loader", 
-    Vector2.new(300, 250)
-);
-local menuwindow = Library.Window(
-    "Window1", 
-    Vector2.new(524, 550)
-);
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/1%20Tokyo%20Lib%20(FIXED)/Tokyo%20Lib%20Source.lua"))({
+    cheatname = "FatalShot",
+    gamename = "Sniper Duels", 
+})
 
-local tab = Window1:Tab("1222121")
-local section = Tab1:Section("edwd", "Left")
+library:init()
+
+с
+
+
+local randomWord = WordList[math.random(#WordList)]
+local Window1 = library.NewWindow({
+    title = "FatalShot | " .. randomWord,
+    size = UDim2.new(0, 510, 0.6, 6)
+})
+
+local AimTab = Window1:AddTab("Aim")
+local TabSettings = library:CreateSettingsTab(Window1)
+local AimSettingsSection = Tab1:AddSection("Aim Settings", 1)
+
+
+Section1:AddToggle({
+    text = "Aim",
+    state = false,
+    risky = true,
+    tooltip = "toooltiptetss",
+    flag = "aim",
+    risky = false,
+    callback = function(v)
+        print("Aim")
+    end
+}):AddBind({
+    enabled = true,
+    text = "Aim Keybind",
+    tooltip = "tooltip1hzhzhzh",
+    mode = "toggle",
+    bind = "M2",
+    flag = "aimkey",
+    state = false,
+    nomouse = false,
+    risky = false,
+    noindicator = false,
+    callback = function(v)
+        print(ValueText, v)
+    end,
+    keycallback = function(v)
+        print("activated")
+    end
+})
