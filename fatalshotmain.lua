a253ea6c02e335bee0b871ea9141972c0e3c7ca9
sadia4ek <kapybara1122@gmail.com>
2025-11-26 21:14:37 +0200

    Update fatalshotmain.lua

--- fatalshotmain.lua
+++ fatalshotmain.lua
@@ -26,24 +26,6 @@ local RandomNameList = {
     "destination: win",
 }
 
-
-local WordList = {
-    "ez clap",
-    "no chance",
-    "outplayed",
-    "gg ez",
-    "too fast",
-    "you can't hit me",
-    "auto win",
-    "skill issue",
-    "next time maybe",
-    "try harder",
-    "no mercy",
-    "too strong",
-    "unstoppable",
-    "dominated",
-}
-
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Other/main/1"))()
 
 library:init()
@@ -75,7 +57,70 @@ local sections = {
 	aimsection = tabs.aatab:AddSection("Aim", 1),
 	fakepitchsection = tabs.aatab:AddSection("Fake Pitch", 2),
 	-- misc tab sections
-	another = tabs.misctab:AddSection("Another", 1),
-	sounds = tabs.misctab:AddSection("Soon...", 2),
-}
+sections.Section1:AddBind({
+    text = "Keybind",
+    flag = "Key_1",
+    nomouse = true,
+    noindicator = true,
+    tooltip = "Tooltip1",
+    mode = "toggle",
+    bind = Enum.KeyCode.Q,
+    risky = false,
+    keycallback = function(v)
+        local trashTalk = {
+            "bro aiming at the moon?",
+            "nice try… but no",
+            "outplayed lol",
+            "bro folding like paper",
+            "you lagging or skill issue?",
+            "I wasn’t even trying",
+            "my pet rock plays better",
+            "still loading?",
+            "this is embarrassing",
+            "why so slow?",
+            "NPC detected",
+            "bot behavior",
+            "bro forgot to enable brain",
+            "cry about it",
+            "rent free",
+            "you good? hello?",
+            "bro blinked and died",
+            "touch grass pls",
+            "your aim is on vacation",
+            "deleted from existence",
+            "too easy",
+            "bro thinks he's him",
+            "respawn and try again",
+            "what was the plan??",
+            "you're not serious right?",
+            "bro playing on toaster",
+            "outskilled",
+            "nah this can't be real",
+            "thanks for the free kill",
+            "matchmaking did you dirty",
+            "bro panicked",
+            "GG = get good again",
+            "who are you aiming at??",
+            "I’ll wait until you hit one shot",
+            "lost in the sauce",
+            "bro moving in slow motion",
+            "don’t uninstall pls 😭",
+            "no way you missed that",
+            "your reactions expired",
+            "I’m playing with 1 FPS and still winning",
+            "bro fighting ghosts",
+            "aim.exe has stopped working",
+            "I sneezed and won",
+            "how are you losing this?",
+            "bro playing with oven controls",
+        }
+
+        local ChatEvent = game.ReplicatedStorage.Chat.ChatEvents.SayMessage
+        local randomIndex = math.random(1, #trashTalk)
+        local message = trashTalk[randomIndex]
+        ChatEvent:FireServer(message, "All")
+		print("trashtalk sent")
+    end
+})
+
 
